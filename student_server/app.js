8403b409a36d96ec12d47d4237799e93a1dd55b3
Michael Allan <mikelallan1307@gmail.com>
2018-04-07 20:13:47 -0600

    Replaced default student server with node

---
+++ student_server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '/js')));
+app.use(express.static(path.join(__dirname, '..', 'ether_contracts/build/contracts/')));
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
